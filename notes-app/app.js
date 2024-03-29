0d8c39b8c55863e21723fc6c6c1f146fcb99f916
azar <azar24feb@gmail.com>
2024-03-29 13:12:02 +0530

    yargs demo

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,94 @@
-const command =
+const yargs = require('yargs')
+
+//customize yargs version
+yargs.version('1.1.0')
+
+//add, remove, read, list notes
+
+//create Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title : ' + argv.title)
+        console.log('Body : ' + argv.body)
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note!',
+    handler: function(){
+        console.log('Remove a note!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note!',
+    handler: function(){
+        console.log('Read a note!')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes!',
+    handler: function(){
+        console.log('List all notes!')
+    }
+})
+
+// console.log(yargs.argv);
+yargs.parse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
+
+//using command line arguments
+const command = process.argv[2]
+
+if (command === 'add') {
+    console.log('Adding Note!');
+} else if (command === 'remove') {
+    console.log('Removing Note!');
+}
+
+//using validator
 const validator = require('validator')
 console.log(validator.isEmail('abc@cde.fgh'))
 
