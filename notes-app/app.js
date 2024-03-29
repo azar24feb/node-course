96f0a82a9d03a09361e4c5ec5796e522cb15ebc4
azar <azar24feb@gmail.com>
2024-03-29 11:35:14 +0530

    abc

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,15 @@
-
+const command =
 
 /*
 const validator = require('validator')
 console.log(validator.isEmail('abc@cde.fgh'))
 
 const chalk = require('chalk')
+const notes = require('./notes')
+
+console.log(notes())
 console.log(chalk.green.bgRed.bold('Success!'))
 console.log(chalk.green('Success!'))
+
+console.log(process.argv)
 */
